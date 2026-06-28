f44496eca61bc79d9db634fffa1f73b0864370e1
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-06-28 17:28:51 +0530

    user model types

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,20 +3,29 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true
+        required: true,
+        maxLength: 50,
+        trim: true
     },
     lastName: {
         type: String,
-        required: true
+        required: true,
+        maxLength: 50,
+        trim: true,
     },
     email: {
         type: String,
         unique: true,
-        required: true
+        required: true,
+        trim: true, 
+        lowercase: true, 
+        minLength: 3,
+        maxLength: 30 
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minLength: 6
     }
 },{
     timestamps: true
