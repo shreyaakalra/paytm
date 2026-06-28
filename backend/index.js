73a045521ff3a24c4684cadd5eea376523d1693c
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-06-28 22:51:58 +0530

    setting up the router

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import connectDB from "./db.js"
+import router from "./routes/index.js"
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
-
 connectDB();
+
+app.use("/api/v1", router);
+// how does it work? 
+// Browser hits → /api/v1/users
+// index.js sees /api/v1 → forwards to router
+// router sees /users → runs the function → sends back JSON
 
 app.listen(port, () => {
     console.log("App is listening at port: 3001");
