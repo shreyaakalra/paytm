0dc7e2cf59364f2fa8de759de937dfed377b4b01
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-06-30 19:10:39 +0530

    transfering money using transactions in mongodb

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -34,6 +34,79 @@ accountRouter.get('/balance', authMiddleware, async(req, res) => {
         })
     }
     
+});
+
+accountRouter.post('/transfer', authMiddleware, async(req, res) => {
+
+    const session = await mongoose.startSession();
+    
+    session.startTransaction();
+
+    try{
+        const { to, amount } = req.body;
+        const fromUserID = req.userId;
+
+        if(!to || !amount || amount<=0){
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "Please provide a valid to and amount."
+            });
+        }
+
+        if(to == fromUserID){
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "bruh you can't send money to yourself."
+            });
+        }
+
+        const fromAccount = await accountSchema.findOne({userId: fromUserID}).session(session);
+
+        if(!fromAccount || fromAccount.balance < amount){
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "Insufficient balance."
+            });
+        }
+
+        const toAccount = await accountSchema.findOne({userId: to}).session(session);
+
+        if(!toAccount){
+            await session.abortTransaction();
+            return res.status(400).json({
+                message: "Receiver account doesn't exist."
+            });
+        }
+
+        await accountSchema.updateOne({userId: fromUserID},{
+            $inc : { balance: -amount }
+        }).session(session);
+
+        await accountSchema.updateOne({ userId: to}, {
+            $inc: { balance: +amount }
+        }).session(session);
+
+        await session.commitTransaction();
+
+        res.status(200).json({
+            message: "Transaction successful"
+        });
+
+    } catch(err){
+        await session.abortTransaction();
+        console.log(err.message);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+
+    } finally {
+        session.endSession();
+    }
+
+
+
+
+
 })
 
 export default accountRouter;
